eca67cd7168087492e94c6b5bd25458e62d01a17
raularino <raulac98@gmail.com>
2020-03-05 18:11:25 +0100

    Añadir algunos modelos

--- RecipesAndFun/models.py
+++ RecipesAndFun/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
 
 
 class receta(models.Model):
-    nombre_receta= models.CharField(max_length=100, unique=True)
+    nombre_receta= models.CharField(max_length=100)
     ingredientes= models.TextField(help_text='Escribe los ingredientes')
     preparacion= models.TextField(verbose_name='Preparacion')
     tiempo_preparacion= models.IntegerField
+    comentarios= models.CharField(help_text='Espacio para comentarios', max_length=1000, null=True)
     puntuacion= models.IntegerField
-    fecha_publicacion= models.DateTimeField(auto_now=True)
+    fecha_publicacion= models.DateTimeField(blank=True, null=True)
     #usuario= models.ForeignKey(User)
 
     def __unicode__(self):
-        return u"%s" % self.name
+        return u"%s" % self.nombre_receta
 
+    def create(self):
+        self.fecha_publicacion=timezone.now()
