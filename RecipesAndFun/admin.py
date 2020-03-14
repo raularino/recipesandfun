82ce1cd8fa9a2ea19fe510c9a788d98e097dcdd7
paulavicente98 <46265541+paulavicente98@users.noreply.github.com>
2020-03-14 12:08:32 +0100

    Solucionado problema modelo Ingrediente

--- RecipesAndFun/admin.py
+++ RecipesAndFun/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import receta
+from .models import receta, ingrediente
 # Register your models here.
 
 admin.site.register(receta)
+admin.site.register(ingrediente)
