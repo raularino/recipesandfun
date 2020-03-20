0a06e6e13fc8c5bd7b35736b153fecb0ac9478fb
raularino <raulac98@gmail.com>
2020-03-20 19:03:38 +0100

    Primer formulario 2.0

--- RecipesAndFun/forms.py
+++ RecipesAndFun/forms.py
@@ -4,4 +4,4 @@ from .models import receta
 class RecetaForm(forms.ModelForm):
     class Meta:
         model=receta
-        fields = ('nombre_receta',)
+        fields = ('nombre_receta','preparacion','ingredientes')
